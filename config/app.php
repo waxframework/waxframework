7b3553ca97c6abab5010af5d61010ec2cf18086b
MD AL AMIN <mdalaminbey@gmail.com>
2023-04-26 11:26:58 +0600

    create migration

--- config/app.php
+++ config/app.php
@@ -4,27 +4,34 @@ defined( 'ABSPATH' ) || exit;
 
 use MyPluginNamespace\App\Http\Middleware\EnsureIsUserAdmin;
 use MyPluginNamespace\App\Providers\MenuServiceProvider;
+use MyPluginNamespace\Database\Migrations\TestMigration;
 
 return [
-    'version'         => '1.0.0',
+    'version'                 => '1.0.0',
 
-    'rest_api'        => [
+    'rest_api'                => [
         'namespace' => 'MyPluginApiNamespace',
         'versions'  => []
     ],
 
-    'ajax_api'        => [
+    'ajax_api'                => [
         'namespace' => 'MyPluginApiNamespace',
         'versions'  => []
     ],
 
-    'providers'       => [],
+    'providers'               => [],
 
-    'admin_providers' => [
-        MenuServiceProvider::class
+    'admin_providers'         => [
+        MenuServiceProvider::class,
     ],
 
-    'middleware'      => [
+    'middleware'              => [
         'admin' => EnsureIsUserAdmin::class
+    ],
+
+    'migration_db_option_key' => 'my_plugin_hook_migrations',
+
+    'migrations'              => [
+        'test-migration' => TestMigration::class,
     ]
 ];
